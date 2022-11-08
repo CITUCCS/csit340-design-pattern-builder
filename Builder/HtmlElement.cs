582862a0012dc9086685b8267896194c452cef45
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-11-08 14:28:27 +0800

    Add sample

---
+++ Builder/HtmlElement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    internal class HtmlElement
+    {
+        public string? Name, Text;
+
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+
+        public HtmlElement() { }
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"<{Name}>");
+            foreach (var element in Elements)
+            {
+                sb.AppendLine(element.ToString());
+            }
+            sb.Append($"</{Name}>");
+
+            return sb.ToString();
+        }
+    }
+}
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+var words = new[] { "hello", "world" };
+var sb = new StringBuilder();
+sb.Clear();
+sb.Append("<ul>");
+foreach (var word in words)
+{
+    sb.AppendFormat("<li>{0}</li>", word);
+}
+sb.Append("</ul>");
+Console.WriteLine(sb);
